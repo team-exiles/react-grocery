b7debfae7cf00e403eb06643378a55b76004fbee
Lucian Lodico <llodico@mac.com>
2023-02-18 15:37:30 -0500

    testing123

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>NutBagz</h2>
       </header>
+      <form className="list">
+        <h2>Grocery List</h2>
+        <ul>
+          <li>I am a list hear me roar.</li>
+          <li>RAWR RAWR.</li>
+        </ul>
+      </form>
     </div>
   );
 }
