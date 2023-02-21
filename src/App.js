ee506e15e91a9f4085dc092f5c1395f5f768a270
Cameron Poulton <Crp2108@hotmail.com>
2023-02-21 13:31:15 -0500

    built move divs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { Homepage } from './components/Homepage2';
 import { useState } from 'react'; 
 import twitter from './twitter.png'; 
 import facebook from './facebook.png';
@@ -32,37 +33,9 @@ function App() {
               </div> 
       
             </div>
-    ) : (
-
-        <section className="homepage"> 
-        <div className="logged-in-user">
-          <h2> Username </h2>
-          <h3> Email </h3>
-        </div>
-
-        <div className="active-lists">
-          <h2>Here are my active lists</h2>
-          <p>List 1</p>
-          <p>List 2</p>
-          <p>List 3</p>
-        </div>
-
-          <div className="action-buttons">
-            <button className="Make-New-Folder"></button>
-            <button className="Make-New-List"></button>
-            <button className="Start-Shopping"></button>
-          </div>
-
-          <div className="folders">
-            <button className="recipes-folder">Recipes</button>
-            <button className="archived-folder">Archived</button>
-          </div>
-        </section>)}
-    </div>
-  );
+    ) : <Homepage/>
+    
 }
-
-
-
-
+</div>)
+}
 export default App;
---
+++ src/Components/Homepage2.js
@@ -0,0 +1,55 @@
+
+export const Homepage = () => {
+    return (
+        <>
+        <section className="homepage"> 
+        <div className="logged-in-user">
+          <h2> Username </h2>
+          <h3> Email </h3>
+        </div>
+
+        <div className="active-lists">
+          <h2>Here are my active lists</h2>
+          <p>List 1</p>
+          <p>List 2</p>
+          <p>List 3</p>
+        </div>
+
+          <div className="action-buttons">
+            <button className="Make-New-Folder"></button>
+            <button className="Make-New-List"></button>
+            <button className="Start-Shopping"></button>
+          </div>
+
+          <div className="folders">
+            <button className="recipes-folder">Recipes</button>
+            <button className="archived-folder">Archived</button>
+          </div>
+        </section>        <section className="homepage"> 
+        <div className="logged-in-user">
+          <h2> Username </h2>
+          <h3> Email </h3>
+        </div>
+
+        <div className="active-lists">
+          <h2>Here are my active lists</h2>
+          <p>List 1</p>
+          <p>List 2</p>
+          <p>List 3</p>
+        </div>
+
+          <div className="action-buttons">
+            <button className="Make-New-Folder"></button>
+            <button className="Make-New-List"></button>
+            <button className="Start-Shopping"></button>
+          </div>
+
+          <div className="folders">
+            <button className="recipes-folder">Recipes</button>
+            <button className="archived-folder">Archived</button>
+          </div>
+        </section>
+        
+        </>
+        )}
+  
