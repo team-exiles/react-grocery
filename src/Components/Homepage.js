d199d7626a498e0230d4caafd786e2c5639ab2f3
Lucian Lodico <llodico@mac.com>
2023-02-21 11:08:26 -0500

    route to homepage works

---
+++ src/Components/Homepage.js
@@ -0,0 +1,14 @@
+import {Link} from 'react-router-dom'
+
+export const Homepage = () => {
+    return (
+        <div className="homepage">
+            <h2>All rightie roo, let's crack us up some big, hefty nutz.</h2>
+            <ul>
+                <li>I am a list, hear me roar.</li>
+                <li>Rawr Rawr.</li>
+            </ul>
+            <button><Link to="/Create" path="relative" className="createbutt">Pen List</Link></button>
+        </div>
+    )
+}
