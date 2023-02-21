14d4b78128767a4e2aaf945569010d4883e3d55a
Lucian Lodico <llodico@mac.com>
2023-02-20 21:05:55 -0500

    switched to responsive login

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
 
   const [user, setUser] = useState(false)
 
+
   return (
     <div className="App">
       <header className="App-header">
@@ -15,37 +16,37 @@ function App() {
       </header>
 
       {!user ? (
-      <div className="homepage"> 
-      <h2>My List</h2>
-      <form>
-        <ul>
-          <li>I am a list, hear me roar.</li>
-          <li>Rawr Rawr.</li>
-        </ul>
-      </form>
-      <button onClick={setUser}>Logout</button>
-      </div> 
+              <div> 
+              <h3>Login</h3>
+      
+              <div className="social-login">
+      
+              <div className="Twitter">
+              <button onClick={setUser} className="social-button"><img src={twitter} className="robin" alt="Twitter" /></button> 
+              </div>
+      
+              <div className="Facebook">
+              <button onClick={setUser} className="social-button"><img src={facebook} className="zuckie" alt="Facebook" /></button>
+              </div>
+      
+              <div className="Google">
+              <button onClick={setUser} className="social-button"><img src={google} className="goog" alt="Google" /></button> 
+              </div>   
+                
+              </div> 
+      
+            </div>
     ) : (
-      <div> 
-        <h3>Login</h3>
-
-        <div className="social-login">
-
-        <div className="Twitter">
-        <button onClick={!setUser} className="social-button"><img src={twitter} className="robin" alt="Twitter" /></button> 
-        </div>
-
-        <div className="Facebook">
-        <button onClick={!setUser} className="social-button"><img src={facebook} className="zuckie" alt="Facebook" /></button>
-        </div>
-
-        <div className="Google">
-        <button onClick={!setUser} className="social-button"><img src={google} className="goog" alt="Google" /></button> 
-        </div>   
-          
-        </div> 
-
-      </div>)}
+        <div className="homepage"> 
+        <h2>My List</h2>
+        <form>
+          <ul>
+            <li>I am a list, hear me roar.</li>
+            <li>Rawr Rawr.</li>
+          </ul>
+        </form>
+        <button>Logout</button>
+        </div>)}
 
     </div>
   );
