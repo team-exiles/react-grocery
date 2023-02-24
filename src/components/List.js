0088d85ab7d43f0ebb553c243d5a6295af2fd437
Cameron Poulton <Crp2108@hotmail.com>
2023-02-24 13:33:23 -0500

    cancel button navigates back to homepage and resets states

--- src/components/List.js
+++ src/components/List.js
@@ -7,15 +7,23 @@ export const CreateList = () => {
   // const [listTitle, setListTitle] = useState("Test");
   const [items, setItems] = useState([]);
   const location = useLocation();
-
+  const navigate = useNavigate("");
+  
   console.log(location, " useLocation hook");
 
   const title = location.state?.title;
 
+
+  const handleCancel = (event) => {
+    event.preventDefault()
+    setItems([])
+    navigate("/")
+  }
+
   return (
     <div className="list-display">
       <div className="title-bar">
-        <button className="cancel-list">Cancel</button>
+        <button className="cancel-list" onClick={handleCancel}>Cancel</button>
         <h1>{title}</h1>
         <button>Share</button>
         <button>Edit</button>
